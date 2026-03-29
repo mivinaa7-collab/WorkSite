8da782c24bf20f1bc99a5d9ab65f3a96d3632515
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-30 02:39:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def link_page(link_id):
         price=price,
         link_id=link_id
     )
-
+import os 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
