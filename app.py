9b5fb78f2303c34078ebf41b81ca0db4c6b49f28
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-30 02:22:18 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_conn():
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+@app.route("/link/<int:link_id>")
+def link_page(link_id):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT project, price FROM links WHERE id = %s",
+        (link_id,)
+    )
+
+    data = cur.fetchone()
+    conn.close()
+
+    if not data:
+        return "Ошибка"
+
+    project, price = data
+
+    return render_template(
+        "privat.html",
+        price=price,
+        link_id=link_id
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
